fc2b3686ea31f8797a688d633ba66ef918eec0af
Harsh <harshdatt17@gmail.com>
2022-11-06 16:15:25 +0530

    Health Check - frontend setup

---
+++ Frontend/healthcheck/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='text-xl font-bold text-center'>Health Check in making</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
